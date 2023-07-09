abe4fcc7fe3d5e91721c9ab9f7d4820fa21d8f43
Jerrod Putman <jerrodputman@users.noreply.github.com>
2023-07-09 16:45:29 -0700

    Adds GitHub Action for Linux tests

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftNES",
-    platforms: [.macOS(.v13), .iOS(.v17), .tvOS(.v17)],
+    platforms: [.macOS(.v13), .iOS(.v16), .tvOS(.v16)],
     products: [
         .library(
             name: "SwiftNES",
